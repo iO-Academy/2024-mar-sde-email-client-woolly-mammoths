d3503d96e918579dc8a03844b56499221378d3ff
Louis <louisprotano@hotmail.com>
2024-04-29 13:58:25 +0100

    email item base layout done

--- src/components/EmailItem/EmailItem.jsx
+++ src/components/EmailItem/EmailItem.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 
 function EmailItem({myStyle = "unread"}) {
 
-    cs = 'bg-white p-[10px] border-2 border-black w-[300px]'
+    let cs = 'bg-white p-[10px] border-2 border-black w-[300px] hover:bg-blue-500'
 
     if (myStyle === "read"){
-        cs = 'bg-gray-300 p-[10px] border-2 border-black'
+        cs = 'bg-gray-700 p-[10px] border-2 border-black text-white hover:bg-blue-500 '
     }
 
   return (
     <div className={cs}>
-        <p>content</p>
+        <div className='flex justify-between'>
+            <div className='flex flex-col'>
+                <p>name</p>
+                <p>subject</p>
+                <p>preview...</p>
+            </div>
+            <div className='flex flex-col'>
+                <p>DD/MM/YYYY</p>
+            </div>
+            
+        </div>
 
     </div>
   )
--- src/components/EmailList/EmailList.jsx
+++ src/components/EmailList/EmailList.jsx
@@ -3,10 +3,11 @@ import EmailItem from "../EmailItem/EmailItem"
 function EmmailList() {
 
     return (
-      <div className='flex flex-col-reverse'>
-          <EmailItem myStyle="read"/>
-          <EmailItem myStyle="read"/>
-          <EmailItem myStyle="unread"/>
+      <div className='flex flex-col-reverse bg-gray-800 w-[300px]'>
+            <EmailItem myStyle="unread"/>
+            <EmailItem myStyle="read"/>
+            <EmailItem myStyle="read"/>
+          
   
       </div>
     )
