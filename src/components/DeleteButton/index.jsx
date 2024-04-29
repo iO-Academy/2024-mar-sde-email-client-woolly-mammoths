ccb90ddc75b87f8cbffe38f1e02f65cd7291154d
Ben <benblyth590@googlemail.com>
2024-04-29 16:57:51 +0100

    reply and delete button

---
+++ src/components/DeleteButton/index.jsx
@@ -0,0 +1,11 @@
+const DeleteButton = ({handleClick}) => {
+
+
+    return (
+
+        <button className="bg-red-600 text-white rounded-sm" onClick={handleClick}>Delete</button>
+
+    )
+}
+
+export default DeleteButton
