4157f02b7eba0b893ef9827aed74f6bac9b18ce8
Louis <louisprotano@hotmail.com>
2024-04-29 11:51:18 +0100

    email item start

---
+++ src/components/EmailItem/EmailItem.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function EmailItem() {
+
+  return (
+    <div className='bg-gray-300 p-[10px]'>
+        
+
+    </div>
+  )
+}
+
+export default EmailItem
