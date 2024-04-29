fd1297293325298a7dc2143766f37d4acc372f0b
Bronwen <bronwen@luxi.dev>
2024-04-29 14:31:59 +0100

    inbox page layout sorted

--- src/components/Inbox/index.jsx
+++ src/components/Inbox/index.jsx
@@ -1,8 +1,9 @@
 
 const Inbox = () => {
     return (
-        <div>
-
+        <div className="flex">
+            <div className="w-2/4">Email List Component</div>
+            <div className="w-2/4">Email string component</div>
         </div>
     )
 }
