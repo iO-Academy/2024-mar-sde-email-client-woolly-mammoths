dcdfe71dbffbe1e14d8c0622124565335faea66b
Laurence <lozrecs@hotmail.co.uk>
2024-04-29 15:11:53 +0100

    correction to Header

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,7 +4,7 @@ function Header() {
    
       return (
          <>
-            <header className="bg-gray-500 py-5 py-2.5 px-2.5 text-white flex space-x justify-between items-center">
+            <header className="bg-gray-500 py-2.5 px-2.5 text-white flex space-x justify-between items-center">
                <h1 className="text-2xl">Email Client</h1>
                <div className="username flex items-center">
                <span><i className="fa-solid fa-user text-black border-black border p-1 rounded-full  "></i></span>
