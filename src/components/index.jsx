347f1fae3845cb906299318d1523c7a32f028d2c
Laurence <lozrecs@hotmail.co.uk>
2024-04-29 14:55:47 +0100

    uploaded Header

---
+++ src/components/index.jsx
@@ -0,0 +1,20 @@
+
+
+function Header() {
+   
+      return (
+         <>
+            <header className="bg-gray-500 py-5 py-2.5 px-2.5 text-white flex space-x justify-between items-center">
+               <h1 className="text-2xl">Email Client</h1>
+               <div className="username flex items-center">
+               <span><i className="fa-solid fa-user text-black border-black border p-1 rounded-full  "></i></span>
+               <h4 className="text-sm ml-1.5">User Name</h4>
+               </div>
+            </header>
+         </>
+
+      )
+   
+}
+
+export default Header;
