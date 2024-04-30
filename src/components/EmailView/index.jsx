20463b75f3d4464b074238f316512953f1c81c6d
Louis <louisprotano@hotmail.com>
2024-04-30 16:20:42 +0100

    changed emailView css a bit

--- src/components/EmailView/index.jsx
+++ src/components/EmailView/index.jsx
@@ -32,15 +32,15 @@ const EmailView = ({eName, eDate, eAddress, eSubject, eBody}) => {
 
 return (
     <div>
-        <div className="border-t-2 border-b-2 mx-5 my-5 w-[500px] h-[500px]">
+        <div className="border-t-2 border-b-2 mx-5 my-5 w-[850px] h-[500px]">
             <div className="flex justify-between pt-5">
-                <h2 className="text-xl">{name}</h2>
+                <h2 className="text-xl font-semibold">{name}</h2>
                 <div className="font-bold text-xs">
                     {date}
                 </div>
             </div>
-            <p className="text-xs">{address}</p>
-            <h1 className="text-2xl">{subject}</h1>
+            <p className="text-xs font-semibold">{address}</p>
+            <h1 className="text-2xl font-bold">{subject}</h1>
             <div className="pt-8 pb-2">
                 {body}
             </div>
