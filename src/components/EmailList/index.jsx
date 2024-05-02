70ac0e87276ce50ddafa9d60c96428a3779d4266
Bronwen <bronwen@luxi.dev>
2024-05-02 15:25:45 +0100

    logic working

--- src/components/EmailList/index.jsx
+++ src/components/EmailList/index.jsx
@@ -5,30 +5,23 @@ import EmailView from "../EmailView";
 function EmailList() {
 
     const [emailArray, setEmailArray] = useState(false);
-    const [emailID, setEmailID] = useState(0);
-    const [emailRead, setEmailRead] = useState(0);
     const [refreshJson, setRefreshJson] = useState(false);
+    const [currentEmail, setCurrentEmail] = useState('');
+    const [buttonClass, setButtonClass] = useState("hidden");
+    const [currentID, setCurrentID] = useState(0)
+    
 
-    //To pass into email view
-    const [emailAddress, setEmailAddress] = useState("");
-    const [emailSubject, setEmailSubject] = useState("");
-    const [emailName, setEmailName] = useState("");
-    const [emailDate, setEmailDate] = useState("");
-    const [emailBody, setEmailBody] = useState("");
-    const [emailData, setEmailData] = useState([]);
-   
     useEffect(() => {
         fetch("https://email-client-api.dev.io-academy.uk/emails")
         .then(response => response.json())
-        .then(data => {
-            console.log(data);
+        .then((data) => {
             setEmailArray(data);
             setRefreshJson(false);
-            return(
-                emailArray
-            )
+            return emailArray;
         })
     }, [refreshJson])
+
+ 
 
 
 
@@ -41,85 +34,37 @@ function EmailList() {
         
     }
 
-    const [filteredEmailItems, setFilteredEmailItems] = useState(emailArray);
 
-    const handleSearch = (searchQuery) => {
-        const filteredItems = emailArray.filter(email => {
-            return email.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-                email.subject.toLowerCase().includes(searchQuery.toLowerCase()) ||
-                email.preview.toLowerCase().includes(searchQuery.toLowerCase()) ||
-                email.date_created.includes(searchQuery);
-        });
-        setFilteredEmailItems(filteredItems);
-        setemailArray(filteredItems);
-        console.log("working");
-    };
-
-    const openEmail = (event) => {
-        
-
-        setRead(event.currentTarget.dataset.id);
-
-        if ((emailBody === "") || (emailBody != event.currentTarget.dataset.body)){
-            console.log('ID: ' + event.currentTarget.dataset.id);
-            console.log('DATA: ' + event.currentTarget.dataset.content)
-            setEmailID(event.currentTarget.dataset.id);
-            setEmailData(event.currentTarget.dataset.content);
-            console.log('READ: ' + event.currentTarget.dataset.read);
-            setEmailRead(event.currentTarget.dataset.read);
-
-            //for EmailView
-            console.log('NAME: ' + event.currentTarget.dataset.name);
-            setEmailName(event.currentTarget.dataset.name);
-            console.log('DATE: ' + event.currentTarget.dataset.date);
-            setEmailDate(event.currentTarget.dataset.date);
-            console.log('ADDRESS: ' + event.currentTarget.dataset.address);
-            setEmailAddress(event.currentTarget.dataset.address);
-            console.log('SUBJECT: ' + event.currentTarget.dataset.subject);
-            setEmailSubject(event.currentTarget.dataset.subject);
-            console.log('BODY: ' + event.currentTarget.dataset.body);
-            setEmailBody(event.currentTarget.dataset.body);
-
-            setButtonClass("");
-        }
-        else {
-            closeEmail();
-        }
-
-    }
-
-    function closeEmail() {
-        setEmailName("");
-        setEmailDate("");
-        setEmailAddress("");
-        setEmailSubject("");
-        setEmailBody("");
-        setButtonClass("hidden");
-    }
+    // const openEmail = (event) => {
+    //     setRead(event.currentTarget.dataset.id);
+    //     console.log(event.currentTarget.dataset.id)
+    //     setCurrentID(event.currentTarget.dataset.id);
+    // }
 
     return (
         <div>
         
             <div className="flex w-full justify-between">
-                <div className="overflow-scroll max-h-screen w-2/6">
+                <div className="overflow-scroll max-h-screen w-2/6 border-2 border-black">
                 {
                     emailArray &&
                     <div>
                         {emailArray.data.map(email => {
                             return (
-                                <EmailItem data={email} address={email.email} name={email.name} subject={email.subject} body={email.body} date={email.date_created} id={email.id} myFunction={openEmail} read={email.read}/>
+                                <EmailItem data={email} key={email.id} setCurrentId={setCurrentID} />
                             )
                         })}
                     </div>
                 }
                 </div>
 
-                <EmailView eName={emailName} eDate={emailDate} eAddress={emailAddress}  eSubject={emailSubject} eBody={emailBody} myFunction={closeEmail} buttonClass={buttonClass}/>
+                <EmailView id={currentID} />
+                   
                 
             </div>
         </div>
     ) 
     
-}  
+}
 
 export default EmailList
--- src/components/EmailView/index.jsx
+++ src/components/EmailView/index.jsx
@@ -1,26 +1,38 @@
-const EmailView = ({eName, eDate, eAddress, eSubject, eBody, myFunction, buttonClass}) => {
+import { useState, useEffect } from "react"
 
-    let name = eName;
-    let date = eDate;
-    let address = eAddress;
-    let subject = eSubject;
-    let body = eBody;
-    let bcl = buttonClass;
+const EmailView = ({id}) => {
 
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        console.log('hhhhh')
+        fetch("https://email-client-api.dev.io-academy.uk/emails/" + id)
+        .then(response => response.json())
+        .then(data2 => {
+            setData(data2.data.email);
+        })
+    },[id])
+
+    console.log(data)
 return (
     <div className="border-t-2 border-b-2 mx-5 my-5 w-4/6 h-fit">
+        {data && (
+        <>
         <div className="flex justify-between pt-3">
-            <h2 className="text-xl font-semibold">{name}</h2>
+            <h2 className="text-xl font-semibold">{data.name}</h2>
             <div className="font-bold text-xs">
-                {(new Date(date).toLocaleDateString())}
+                {(new Date(data.date).toLocaleDateString())}
             </div>
+            
         </div>
-        <p className="text-xs font-semibold my-2">{address}</p>
-        <h1 className="text-2xl font-bold">{subject}</h1>
+        <p className="text-xs font-semibold my-2">{data.address}</p>
+        <h1 className="text-2xl font-bold">{data.subject}</h1>
         <div className="pt-8 pb-2">
-            {body}
+            {data.body}
         </div>
-    </div>
+        </>
+    )}
+        </div>
     
 )
 }
